3c8618a1a6c8b5b5a3c3d5d76d6b6174151f65df
David Harrigan <dharrigan@gmail.com>
2021-05-06 10:39:04 +0100

    chore: bump deps

--- src/startrek/base/api/starship/specs.clj
+++ src/startrek/base/api/starship/specs.clj
@@ -6,13 +6,14 @@
 (def should-be-a-uuid "should be a type 4 UUID")
 (def between-1-and-15 "should be between 1 and 15 characters")
 (def between-1-and-20 "should be between 1 and 20 characters")
+(def between-1-and-30 "should be between 1 and 30 characters")
 (def between-5-and-30 "should be between 5 and 30 characters")
 (def between-2000-and-3000 "should be between 2000 and 3000")
 
 (def create [:map
              {:closed true}
              [:captain [:string {:min 1 :max 20 :error/message between-1-and-20}]]
-             [:affiliation [:string {:min 1 :max 30 :error/message between-5-and-30}]]
+             [:affiliation [:string {:min 1 :max 30 :error/message between-1-and-30}]]
              [:launched [:int {:min 2000 :max 3000 :error/message between-2000-and-3000}]]
              [:class [:string {:min 1 :max 15 :error/message between-1-and-15}]]
              [:registry [:string {:min 1 :max 15 :error/message between-1-and-15}]]
